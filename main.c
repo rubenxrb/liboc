e998f62de743693b2b2f20c23989db62e6c6c2da
Ruben Romero <rubenxrb@gmail.com>
2017-02-20 00:58:00 -0800

    playing around with a main program to call objects created by different abstract datas

--- main.c
+++ main.c
@@ -5,12 +5,16 @@
 
 int main ()
 {
-	char *str;
-	void *obj;
+	void *s1;
+	void *s2;
 
-	obj = new(String, "This is my sample text\n");
-	str = (char *)clone(obj);
-	printf("str = [%s]\n", str);
-	delete(obj);
+	s1 = new(String, "This is my sample text\n");
+	s2 = new(String, "This is another example\n");
+	printf("Size of s1: %zu\n", sizeOf(s1));
+	if (string_differ(s1, s2))
+		printf("strings are diff\n");
+	printf("Size of s2: %zu\n", sizeOf(s2));
+	delete(s1);
+	delete(s2);
 	return 0;
 }
